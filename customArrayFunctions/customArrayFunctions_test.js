7605b1ed4d6d3b452bb60acf57213c067f68f230
Maks <maks_zhukov_97@mail.ru>
2018-03-10 16:01:36 +0300

    customArrayFunctions 1.3

--- customArrayFunctions/customArrayFunctions_test.js
+++ customArrayFunctions/customArrayFunctions_test.js
@@ -4,28 +4,28 @@ describe("Custom Array Functions", function() {
     it("Method 'take' must return first element form array", function() {
         expect(customArrayFunctions.take(inputArrayTest, 1)).toEqual([2]);
     });
-    it("Method 'take' takes first argument boolean", function() {
+    it("Method 'take' return undefined when first argument boolean", function() {
         expect(customArrayFunctions.take(false, 1)).toEqual(undefined);
     });
-    it("Method 'skip' takes first argument number", function() {
+    it("Method 'skip' return undefined when first argument number", function() {
         expect(customArrayFunctions.skip(5, 1)).toEqual(undefined);
     });
-    it("Method 'map' takes first argument string", function() {
+    it("Method 'map' return undefined when first argument string", function() {
         expect(customArrayFunctions.map("fwq", 1)).toEqual(undefined);
     });
-    it("Method 'reduce' takes first argument undefined", function() {
+    it("Method 'reduce' return undefined when first argument undefined", function() {
         expect(customArrayFunctions.reduce(undefined, 1, 1)).toEqual(undefined);
     });
-    it("Method 'filter' takes first argument object", function() {
+    it("Method 'filter' return undefined when first argument object", function() {
         expect(customArrayFunctions.filter({}, 1)).toEqual(undefined);
     });
-    it("Method 'foreach' takes first argument null", function() {
+    it("Method 'foreach' return undefined when first argument null", function() {
         expect(customArrayFunctions.foreach(null, 1)).toEqual(undefined);
     });
-    it("Method 'reduce' takes an array not only from numbers", function() {
+    it("Method 'reduce' return undefined when first argument is an array not only from numbers", function() {
         expect(customArrayFunctions.reduce(inputArrayTestWithChar, (index, element) => { return element; }, 1)).toEqual(undefined);
     });
-    it("Method 'take' do not take arguments", function() {
+    it("Method 'take' return undefined when do not take arguments", function() {
         expect(customArrayFunctions.take()).toEqual(undefined);
     });
 });
