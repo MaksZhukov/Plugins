868034a5bcbc4394243a71df9958cb2357bf534e
Maks <maks_zhukov_97@mail.ru>
2018-04-06 23:41:46 +0300

    Fix ES6 1.3

--- customArrayFunctions/customArrayFunctions.js
+++ customArrayFunctions/customArrayFunctions.js
@@ -1,111 +1,111 @@
 "use strict";
-var customArrayFunctions = (function() {
-    function isArrayNumber(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            if (typeof arr[i] !== "number") {
-                return false;
-            }
-        }
-        return true;
+const customArrayFunctions = (function() {
+  function isArrayNumber(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "number") {
+        return false;
+      }
     }
+    return true;
+  }
 
-    function take(arr, n) {
-        if (Array.isArray(arr)) {
-            return arr.slice(0, n);
-        }
+  function take(arr, n) {
+    if (Array.isArray(arr)) {
+      return arr.slice(0, n);
     }
+  }
 
-    function skip(arr, n) {
-        if (Array.isArray(arr)) {
-            return arr.slice(n, arr.length);
-        }
+  function skip(arr, n) {
+    if (Array.isArray(arr)) {
+      return arr.slice(n, arr.length);
     }
+  }
 
-    function map(arr, callback) {
-        if (Array.isArray(arr)) {
-            var length = arr.length,
-                newarr = [];
-            for (var i = 0; i < length; i = i + 1) {
-                newarr.push(callback(i, arr[i]));
-            }
-            return newarr;
-        }
+  function map(arr, callback) {
+    if (Array.isArray(arr)) {
+      var length = arr.length,
+        newarr = [];
+      for (var i = 0; i < length; i = i + 1) {
+        newarr.push(callback(i, arr[i]));
+      }
+      return newarr;
     }
+  }
 
-    function reduce(arr, callback, val) {
-        if (Array.isArray(arr) && isArrayNumber(arr)) {
-            var length = arr.length,
-                i = 0,
-                value;
-            value = val === undefined ? arr[i++] : val;
-            for (; i < length; i++) {
-                value = callback(value, arr[i]);
-            }
-            return value;
-        }
+  function reduce(arr, callback, val) {
+    if (Array.isArray(arr) && isArrayNumber(arr)) {
+      var length = arr.length,
+        i = 0,
+        value;
+      value = val === undefined ? arr[i++] : val;
+      for (; i < length; i++) {
+        value = callback(value, arr[i]);
+      }
+      return value;
     }
+  }
 
-    function filter(arr, callback) {
-        if (Array.isArray(arr)) {
-            var length = arr.length,
-                newarr = [];
-            for (var i = 0; i < length; i = i + 1) {
-                if (callback(i, arr[i])) {
-                    newarr.push(arr[i]);
-                }
-            }
-            return newarr;
+  function filter(arr, callback) {
+    if (Array.isArray(arr)) {
+      var length = arr.length,
+        newarr = [];
+      for (var i = 0; i < length; i = i + 1) {
+        if (callback(i, arr[i])) {
+          newarr.push(arr[i]);
         }
+      }
+      return newarr;
     }
+  }
 
-    function foreach(arr, callback) {
-        if (Array.isArray(arr)) {
-            var length = arr.length;
-            for (var i = 0; i < length; i = i + 1) {
-                callback(i, arr[i]);
-            }
-        }
+  function foreach(arr, callback) {
+    if (Array.isArray(arr)) {
+      var length = arr.length;
+      for (var i = 0; i < length; i = i + 1) {
+        callback(i, arr[i]);
+      }
     }
-
-    function chain(arr) {
-        function wrapChain(func) {
-            return function(...args) {
-                arr = func.call(null, arr, ...args)
-                return this;
-            }
-        }
-        return {
-            take: wrapChain(self.take),
-            skip: wrapChain(self.skip),
-            map: wrapChain(self.map),
-            filter: wrapChain(self.filter),
-            value: () => arr
-        };
+  }
+  function chain(arr) {
+    function wrapChain(func) {
+      return function(...args) {
+        arr = func.call(null, arr, ...args);
+        return this;
+      };
     }
-    var self = {
-        take: take,
-        skip: skip,
-        map: map,
-        reduce: reduce,
-        filter: filter,
-        foreach: foreach,
-        chain: chain
+    return {
+      take: wrapChain(self.take),
+      skip: wrapChain(self.skip),
+      map: wrapChain(self.map),
+      filter: wrapChain(self.filter),
+      value: () => arr
     };
-
-    return self;
-})();
-var sum = (function() {
-    let cash = {};
+  }
+  function sum() {
+    var cash = {};
     return (arr, start, end) => {
-        var result = 0,
-            key = arr.toString()+start+end;
-        if (key in cash){
-            return cash[key];
-        }
-        for (let i = start - 1; i < end; i++) {
-            result += arr[i];
-        }
-        cash[key]= result;
-        return result;
+      var result = 0,
+        key = arr.toString() + start + end;
+      if (key in cash) {
+        return cash[key];
+      }
+      for (let i = start - 1; i < end; i++) {
+        result += arr[i];
+      }
+      cash[key] = result;
+      return result;
     };
+  }
+  var self = {
+    take: take,
+    skip: skip,
+    map: map,
+    reduce: reduce,
+    filter: filter,
+    foreach: foreach,
+    chain: chain,
+    sum: new sum()
+  };
+
+  return self;
 })();
--- customArrayFunctions/customArrayFunctions_es6.js
+++ customArrayFunctions/customArrayFunctions_es6.js
@@ -1,102 +1,102 @@
 class CustomArrayFunctions {
-    isArrayNumber(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            if (typeof arr[i] !== "number") {
-                return false;
-            }
-        }
-        return true;
+	constructor (){
+		this.sum = this.sum();
+	}
+  isArrayNumber(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "number") {
+        return false;
+      }
     }
-    take(arr, n) {
-        if (Array.isArray(arr)) {
-            return arr.slice(0, n);
-        }
+    return true;
+  }
+  take(arr, n) {
+    if (Array.isArray(arr)) {
+      return arr.slice(0, n);
     }
+  }
 
-    skip(arr, n) {
-        if (Array.isArray(arr)) {
-            return arr.slice(n, arr.length);
-        }
+  skip(arr, n) {
+    if (Array.isArray(arr)) {
+      return arr.slice(n, arr.length);
     }
+  }
 
-    map(arr, callback) {
-        if (Array.isArray(arr)) {
-            let length = arr.length,
-                newarr = [];
-            for (let i = 0; i < length; i = i + 1) {
-                newarr.push(callback(i, arr[i]));
-            }
-            return newarr;
-        }
+  map(arr, callback) {
+    if (Array.isArray(arr)) {
+      let length = arr.length,
+        newarr = [];
+      for (let i = 0; i < length; i = i + 1) {
+        newarr.push(callback(i, arr[i]));
+      }
+      return newarr;
     }
+  }
 
-    reduce(arr, callback, val) {
-        if (Array.isArray(arr) && this.isArrayNumber(arr)) {
-            let length = arr.length,
-                i = 0,
-                value;
-            value = val === undefined ? arr[i++] : val;
-            for (; i < length; i++) {
-                value = callback(value, arr[i]);
-            }
-            return value;
-        }
+  reduce(arr, callback, val) {
+    if (Array.isArray(arr) && this.isArrayNumber(arr)) {
+      let length = arr.length,
+        i = 0,
+        value;
+      value = val === undefined ? arr[i++] : val;
+      for (; i < length; i++) {
+        value = callback(value, arr[i]);
+      }
+      return value;
     }
+  }
 
-    filter(arr, callback) {
-        if (Array.isArray(arr)) {
-            let length = arr.length,
-                newarr = [];
-            for (let i = 0; i < length; i = i + 1) {
-                if (callback(i, arr[i])) {
-                    newarr.push(arr[i]);
-                }
-            }
-            return newarr;
+  filter(arr, callback) {
+    if (Array.isArray(arr)) {
+      let length = arr.length,
+        newarr = [];
+      for (let i = 0; i < length; i = i + 1) {
+        if (callback(i, arr[i])) {
+          newarr.push(arr[i]);
         }
+      }
+      return newarr;
     }
+  }
 
-    foreach(arr, callback) {
-        if (Array.isArray(arr)) {
-            let length = arr.length;
-            for (let i = 0; i < length; i = i + 1) {
-                callback(i, arr[i]);
-            }
-        }
+  foreach(arr, callback) {
+    if (Array.isArray(arr)) {
+      let length = arr.length;
+      for (let i = 0; i < length; i = i + 1) {
+        callback(i, arr[i]);
+      }
     }
-    chain(arr) {
-        let wrapChain = (func) => {
-            return (...args) => {
-                arr = func.call(null, arr, ...args)
-                return this.chain(arr);
-            }
-        }
-        return {
-            take: wrapChain(this.take),
-            skip: wrapChain(this.skip),
-            map: wrapChain(this.map),
-            filter: wrapChain(this.filter),
-            value: () => arr
-        };
-    }
+  }
+  chain(arr) {
+    let wrapChain = func => {
+      return (...args) => {
+        arr = func.call(null, arr, ...args);
+        return this.chain(arr);
+      };
+    };
+    return {
+      take: wrapChain(this.take),
+      skip: wrapChain(this.skip),
+      map: wrapChain(this.map),
+      filter: wrapChain(this.filter),
+      value: () => arr
+    };
+  }
+  sum() {
+		let cash = {};
+		return (arr, start, end) => {
+			let result = 0,
+        key = arr.toString() + start + end;
+      if (key in cash) {
+        return cash[key];
+      }
+      for (let i = start - 1; i < end; i++) {
+        result += arr[i];
+			}
+      cash[key] = result;
+      return result;
+		};
+  }
 }
 
-
-let sum = new (function() {
-		let cash = {};
-    return (arr, start, end) => {
-        let result = 0,
-            key = arr.toString()+start+end;
-        if (key in cash){
-            return cash[key];
-        }
-        for (let i = start - 1; i < end; i++) {
-            result += arr[i];
-        }
-        cash[key]= result;
-        return result;
-    };
-}); 
-
-let customArrayFunctions = new CustomArrayFunctions();
-
+const customArrayFunctions = new CustomArrayFunctions();
